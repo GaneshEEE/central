db98559a682481cc5f8e3a08e76825e54631332a
Dhaya027 <dhayahardik8@gmail.com>
2025-07-13 23:12:17 +0530

    new1 update

--- finalui-main/UI-main/backend/test.py
+++ finalui-main/UI-main/backend/test.py
@@ -2,8 +2,8 @@ import google.generativeai as genai
 import requests
 
 # === YOUR API KEYS ===
-GEMINI_API_KEY = "AIzaSyAPfnYR3v_TsclJMEljtU6vNgcvafDFd78"          # Replace this
-GOOGLE_API_KEY = "AIzaSyBiI_i8JbSsAVFNiaMBcLw_jABTrS_HXr"          # Replace this
+GEMINI_API_KEY = "AIzaSyAPfnYR3v_TsclJMEljtU6vNgcvafDFd"          # Replace this
+GOOGLE_API_KEY = "AIzaSyBiI_i8JbSsAVFNiaMBcLw_jABTrS_HXrA"          # Replace this
 SEARCH_ENGINE_ID = "d35cd327525264522"                              # Replace this
 
 # === CONFIGURE GEMINI ===
